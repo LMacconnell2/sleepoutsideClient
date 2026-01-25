b7011918c8a0b59085393f9e9d3669e3149fb0ec
Yat Lam <DaYlAmYaT@user.noreply.github.com>
2026-01-24 18:11:07 -0700

    fix broken cart

--- src/js/product.ts
+++ src/js/product.ts
@@ -1,12 +1,25 @@
 import type { Product } from "./types.mts";
-import { setLocalStorage } from "./utils.mts";
+import { getLocalStorage, setLocalStorage } from "./utils.mts";
 import { findProductById } from "./productData.mts";
 
 function addProductToCart(product: Product) {
-  setLocalStorage("so-cart", product);
+  let cart = [];
+  try {
+    cart = getLocalStorage("so-cart") || [];
+  }
+  catch{
+    console.error("Error in saving to LocalStorage");
+  }
+  
+  if (!Array.isArray(cart)) {
+    throw new Error("Cart is not an array");
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e: Event) {
+  console.log("Button clicked");
   const target = e.target as HTMLButtonElement;
   if (target.dataset.id) {
     const product = await findProductById(target.dataset.id);
